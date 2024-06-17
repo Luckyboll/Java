e0f32b9628c95f0aa0bf372645dd29105f842a5d
Luckyboll <2041788817@qq.com>
2024-06-17 15:19:29 +0800

    web

--- Basketball/src/main/java/Controller/PlayerController.java
+++ Basketball/src/main/java/Controller/PlayerController.java
@@ -1,14 +1,23 @@
 package Controller;
 
+import Entity.Player;
+import Entity.Result;
+import Service.PlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/player")
 public class PlayerController {
+    @Autowired
+    private PlayerService playerService;
     @GetMapping
-    public void list(){
-
+    public Result<List<Player>> list() {
+        List<Player> list = playerService.findAll();
+        return Result.success(list);
     }
 }
